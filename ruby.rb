4f65079ab701c323cdc172ac596dde9433c98b7c
Maria Henriquez <mariamerhenriquez@gmail.com>
2022-04-02 10:23:27 -0500

    reviewing ruby

---
+++ ruby.rb
@@ -0,0 +1,15 @@
+# To know wich version of Ruby you have installed, write in the terminal
+    # ruby -v
+# Consola intercativa de ruby
+    # irb
+    # asi todo lo que está adentro funciona en código Ruby
+    # Salir de la consola: <exit>
+
+puts "Hola Mariamer"
+# Then you can carry the file to console and write before <ruby>, then it will print the result (will run)
+# Another way its pust ruby followed by the filename in ruby and press enter (I Think its best), but you have to be in the folder
+
+# Variables
+    # remenber de snake case: the name always in lower case and ehen it has more yhan 1 word folwwed by underscore /you can start the name of the var with an underscore
+    # The nature of the variable is that it can change its value    
+# Don´t name a variable with some words, for example: new / 
