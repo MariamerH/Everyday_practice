65ebfbe783ebec41930e2fe2a98e9aed77ef7391
Maria Henriquez <mariamerhenriquez@gmail.com>
2022-04-02 11:04:42 -0500

    learning numbers and methods

--- ruby.rb
+++ ruby.rb
@@ -12,4 +12,23 @@ puts "Hola Mariamer"
 # Variables
     # remenber de snake case: the name always in lower case and ehen it has more yhan 1 word folwwed by underscore /you can start the name of the var with an underscore
     # The nature of the variable is that it can change its value    
-# Don´t name a variable with some words, for example: new / 
+    # Don´t name a variable with some words, for example: new / 
+
+# Numbers
+    # Integer: remenber when you need an exact result with decimal, ypu have to specify the decimal, it means it´s mandatory puts a float or convert, if not the result will be absolut
+    # Float: 
+    # Convert to integer: .to_i
+    # Convert to float: .to_f
+    # Convert to absolute: .abs
+    # Know if it a even (Bollean): .even? => it will return true or false
+    # Know the next number: .next
+
+# Remenber:
+    # Concatenade: it just put a + and join the objects
+    # Interpolate #{} it permits inside of the brakets do a ruby code
+    # Puts add a linebreak, print doesnn´t
+    # \n it will print a line break
+    # \t it will print tab
+    # IO / Input / Output
+    # gets => the user will type something but it´s add a break line, that´s why we use getschomp
+        #getschomp
