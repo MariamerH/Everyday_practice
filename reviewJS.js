2714f8e066e322f7a47948b7c12f68c74c4d2996
Maria Henriquez <mariamerhenriquez@gmail.com>
2022-04-05 12:16:03 -0500

    review comments, variables types and operators

---
+++ reviewJS.js
@@ -0,0 +1,25 @@
+// para iniciar lo que sea en JS, se hace en el html, colocando etiquetas de inicio y cierre con
+// la palabra script
+// <script>....</script>, asi se entiende que todo lo que está dentro de esas etiquetas es JS
+
+/* éste simbolo se usa para comentarios de varias líneas
+puedo seguir escribiendo sin problema, los cerramos a la inversa */
+
+/* Tipos de datos
+Undefined - valor que se asigna por defecto cuando creamos la variable pero no le asignamos valor
+Null
+Boolean
+String
+Symbol
+Number
+Object - estructura que nos ermite relacionar sus partes
+
+Variable: crea un espacio en la memoria para guardar información
+    se define con var seguido de su nombre y le asignamos un valor
+por ej: var number = "suVAlor";
+la llamamos mediante:
+    console.log(nombreVariable);
+
+Tipos de Variable:
+
+*/
